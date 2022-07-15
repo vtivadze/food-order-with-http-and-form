90eeec985d6b18c620198aa0463674cb96de6d54
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-15 08:17:03 +0400

    Apply useRef hook for getting user input

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,27 +1,39 @@
+import { useRef } from 'react';
+
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
+  const nameInputRref = useRef();
+  const streetInputRref = useRef();
+  const postalCodeInputRref = useRef();
+  const cityInputRref = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enterdName = nameInputRref.current.value;
+    const enteredStreet = streetInputRref.current.value;
+    const enterdPostalCode = postalCodeInputRref.current.value;
+    const enterdCity = cityInputRref.current.value;
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRref} />
       </div>
       <div className={classes.control}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input type='text' id='street' ref={streetInputRref} />
       </div>
       <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input type='text' id='postal' ref={postalCodeInputRref} />
       </div>
       <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' id='city' ref={cityInputRref} />
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.onCancel}>
